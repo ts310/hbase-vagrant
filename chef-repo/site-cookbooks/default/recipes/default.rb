9d0553c33653ea3986fef0ae634f6eb7edb4526d
Tsuyoshi Saito <tsuyoshi310@gmail.com>
2016-02-21 02:59:32 +0900

    Install hbase from cloudera cdh5 with yum package hbase-master

--- chef-repo/site-cookbooks/default/recipes/default.rb
+++ chef-repo/site-cookbooks/default/recipes/default.rb
@@ -12,10 +12,20 @@
   end
 end
 
-ruby_block 'service-hbase-master-start-and-enable' do
-  block do
-    %w(enable start).each do |action|
-      resources('service[hbase-master]').run_action(action.to_sym)
-    end
-  end
+remote_file '/tmp/cloudera-cdh-5-0.x86_64.rpm' do
+  source 'http://archive.cloudera.com/cdh5/one-click-install/redhat/6/x86_64/cloudera-cdh-5-0.x86_64.rpm'
+end
+
+package 'cloudera-cdh-5-repo' do
+  action :install
+  provider Chef::Provider::Package::Rpm
+  source '/tmp/cloudera-cdh-5-0.x86_64.rpm'
+end
+
+package 'hbase-master' do
+  action :install
+end
+
+service 'hbase-master' do
+  action [:start, :enable]
 end
